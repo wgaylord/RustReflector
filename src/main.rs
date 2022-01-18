66deacfd63dfb38ae16e1e73663c85d071494b3d
William <chibill110@gmail.com>
2022-01-18 13:03:35 -0600

    Remove all the annoying tabs and replace them with spaces.

--- src/main.rs
+++ src/main.rs
@@ -32,22 +32,22 @@ fn main() {
         clients.retain(|key, value| { //Used for removing clients that timed out.
             response_bytes.clear(); //Clear the response buffer
             let mut keep = true;
-          	match value.ping_time.elapsed() { //Check Elapsed time on the ping of each client.
-       	        Ok(elapsed) => {
-       	            if elapsed.as_secs() > 10 { //If over 10 secs since last PONG send a PING.
-          			    response_bytes.extend([65,67,75,78].iter());
-		                response_bytes.extend(value.callsign.iter());
-		                socket.send_to(&response_bytes,value.socket_addr);
-		                keep = true;
-          		    }
-          		    if elapsed.as_secs() > 60 { //If over 60 sec since last PONG, DISC the cleint and remove it from the hashmap.
-          			    let mut response_bytes = Vec::<u8>::new();
-          			    response_bytes.extend([68,73,83,67].iter());
-		                response_bytes.extend(value.callsign.iter());
-		                socket.send_to(&response_bytes,value.socket_addr);
-		                keep = false;
-          		    }
-          		}	
+            match value.ping_time.elapsed() { //Check Elapsed time on the ping of each client.
+                Ok(elapsed) => {
+                    if elapsed.as_secs() > 10 { //If over 10 secs since last PONG send a PING.
+                        response_bytes.extend([65,67,75,78].iter());
+                        response_bytes.extend(value.callsign.iter());
+                        socket.send_to(&response_bytes,value.socket_addr);
+                        keep = true;
+                    }
+                    if elapsed.as_secs() > 60 { //If over 60 sec since last PONG, DISC the cleint and remove it from the hashmap.
+                        let mut response_bytes = Vec::<u8>::new();
+                        response_bytes.extend([68,73,83,67].iter());
+                        response_bytes.extend(value.callsign.iter());
+                        socket.send_to(&response_bytes,value.socket_addr);
+                        keep = false;
+                    }
+                }    
                 Err(error) => { //Catch any errors. No idea what error could happen but we need to take care of it.
                     // an error occurred!
                     println!("Error: {:?}", error); 
@@ -62,46 +62,46 @@ fn main() {
 fn handle_packet(socket: &UdpSocket,addr:SocketAddr,buf: &[u8],clients: &mut HashMap::<SocketAddr,Client> ) {
     let mut response_bytes = Vec::<u8>::new(); //Buffer for responses.
     match buf {
-	    [67,79,78,78, ..] => { //Handle CONN packets
-	        println!("CONN");
-	        socket.send_to(&[65,67,75,78],addr); //Send ACKN to the client
-	        let client = Client{  //Build a Client struct instance. 
+        [67,79,78,78, ..] => { //Handle CONN packets
+            println!("CONN");
+            socket.send_to(&[65,67,75,78],addr); //Send ACKN to the client
+            let client = Client{  //Build a Client struct instance. 
                 socket_addr: addr,
-    			callsign: [buf[4],buf[5],buf[6],buf[7],buf[8],buf[9]], //Need to find a better way to copy parts of a u8 array in Rust
-		    	module: buf[10],
-		    	ping_time: SystemTime::now(),
-		    	pingged: true
+                callsign: [buf[4],buf[5],buf[6],buf[7],buf[8],buf[9]], //Need to find a better way to copy parts of a u8 array in Rust
+                module: buf[10],
+                ping_time: SystemTime::now(),
+                pingged: true
             };
             clients.insert(addr,client); //Add client to hashmap
-		    response_bytes.extend([65,67,75,78].iter()); //Add PING to response
-		    response_bytes.extend(clients[&addr].callsign.iter()); //Add client callsign
-		    socket.send_to(&response_bytes,addr); //Send Ping
-		},
-	    [80,79,78,71, ..] => { //Handle PONG packets
-	        println!("PONG");
-	        if clients[&addr].pingged { //Did we actually ping this client? Or is it s dumb client just sending PONG?
-	            clients.get_mut(&addr).unwrap().ping_time = SystemTime::now(); //Updating time of last PONG using some hacky stuff to be able to modify struct inside hashmap. :/ Annoying Rust
-	            clients.get_mut(&addr).unwrap().pingged = false; //Update pingged field in same way.
-			} 
-	    },
-	    [68,73,83,67, ..] => { //Handle DISC packets
-	        println!("DISC"); //Got a DISC need to reply with just DISC for client to properly disconnect.
-	        socket.send_to(&[68,73,83,67],addr); //Send DISC
-		    clients.remove(&addr); //Remove client from hashmap. NEED TO DO ERROR CHECKING HERE. Non-existant client disconnecting crashes server.
-	    },
-	    [77,49,55,32, ..] => { //Handle M17 packets
-	        println!("M17");
-	        for (key, value) in &*clients { //Loop over all the clients.
-			    if !(value.socket_addr == addr) { // As long as its not the sender continue to next check.
-			        if value.module == clients[&addr].module{ //Is the client we are looking at on the senders module?
-				        socket.send_to(&buf,value.socket_addr);//Send them the packet. (Should there be any packet rewriting here? Docs make this unclear.)
-			        }
-		        }
-		    }
-	    },
-	    [_] => {println!("WERID PACKET!")}, //Packet with only 1 byte of data
-	    [] => {println!("IMPOSSIBLE PACKET")}, //Packet with no data !?!
-	    [_, ..] => {println!("WERID PACKET!")}, //Packet that doesn't match anything else.
+            response_bytes.extend([65,67,75,78].iter()); //Add PING to response
+            response_bytes.extend(clients[&addr].callsign.iter()); //Add client callsign
+            socket.send_to(&response_bytes,addr); //Send Ping
+        },
+        [80,79,78,71, ..] => { //Handle PONG packets
+            println!("PONG");
+            if clients[&addr].pingged { //Did we actually ping this client? Or is it s dumb client just sending PONG?
+                clients.get_mut(&addr).unwrap().ping_time = SystemTime::now(); //Updating time of last PONG using some hacky stuff to be able to modify struct inside hashmap. :/ Annoying Rust
+                clients.get_mut(&addr).unwrap().pingged = false; //Update pingged field in same way.
+            } 
+        },
+        [68,73,83,67, ..] => { //Handle DISC packets
+            println!("DISC"); //Got a DISC need to reply with just DISC for client to properly disconnect.
+            socket.send_to(&[68,73,83,67],addr); //Send DISC
+            clients.remove(&addr); //Remove client from hashmap. NEED TO DO ERROR CHECKING HERE. Non-existant client disconnecting crashes server.
+        },
+        [77,49,55,32, ..] => { //Handle M17 packets
+            println!("M17");
+            for (key, value) in &*clients { //Loop over all the clients.
+                if !(value.socket_addr == addr) { // As long as its not the sender continue to next check.
+                    if value.module == clients[&addr].module{ //Is the client we are looking at on the senders module?
+                        socket.send_to(&buf,value.socket_addr);//Send them the packet. (Should there be any packet rewriting here? Docs make this unclear.)
+                    }
+                }
+            }
+        },
+        [_] => {println!("WERID PACKET!")}, //Packet with only 1 byte of data
+        [] => {println!("IMPOSSIBLE PACKET")}, //Packet with no data !?!
+        [_, ..] => {println!("WERID PACKET!")}, //Packet that doesn't match anything else.
     }
 }
 
