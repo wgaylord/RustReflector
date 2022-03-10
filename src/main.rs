6ab31d0ea1bf8ebe10c77f99f8bbb8f45d62fc35
William <chibill110@gmail.com>
2022-03-10 14:32:12 -0600

    Migrate dashboard into rust application. Still work in progress on that.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::net::UdpSocket;
 use std::time::SystemTime;
 use std::net::SocketAddr;
 use std::collections::HashMap;
+use std::thread;
+use std::sync::mpsc;
+use std::fs;
+
+use serde::Serialize;
+use tiny_http::{Server, Response};
 
 
 struct Client {
@@ -13,10 +19,22 @@ struct Client {
     talking: bool,
 }
 
+
+#[derive(Serialize)]
+struct ClientInfo {
+    callsign: [u8; 6],
+    module: u8,
+    talking: bool,
+}
+
 impl Client {
     fn pong(&mut self,) -> (){ //Set last pong recv time and set that we are not waiting for a pong.
         self.ping_time = SystemTime::now();
         self.pingged = false;
+    }
+
+    fn ping(&mut self,) -> (){ //Set ping.
+        self.pingged = true;
     }
 
     fn new(sock: SocketAddr, call: [u8; 6], module: u8) -> Client {
@@ -30,53 +48,102 @@ impl Client {
     }
 }
 
+
+
 fn main() {
     println!("Hello, M17 world! Starting Reflector.");
+    println!("Starting UDP Server thread.");
+    let (tx,rx) = mpsc::channel(); //Get inter-thread communication.
+    thread::spawn(|| {udp_thread(tx);});
+    
+    let mut Client_list = Vec::<ClientInfo>::new();
 
-    let socket = UdpSocket::bind("0.0.0.0:17000").expect("couldn't bind to address"); //Get a UDP socket setup
-    let running = true; //Not used yet but will be used for killing the server gracefully later.
-    let mut buf = [0; 54]; //Buffer for holding packets. M17 IP packets are never longer then 54 bytes.
-    let mut clients =  HashMap::<SocketAddr,Client>::new(); //Hashmap to hold Client Socket relations. 
-    let mut response_bytes = Vec::<u8>::new(); //Holds the response to be sent.
+    let server = Server::http("0.0.0.0:3001").unwrap();
+    loop{
+        
+        for request in server.incoming_requests() {
 
-    while running {
-        let (_number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data"); //Recv bytes from the socket.
-        //If the message is to long for the buffer the extra bytes are dropped.
-                                        
-        handle_packet(&socket,src_addr,&buf,&mut clients); //Call out packet handler. 
-        //Borrow out socket,buffer and client hashmap. Just give it the src_addr.
-
-        clients.retain(|_key, value| { //Used for removing clients that timed out.
-            response_bytes.clear(); //Clear the response buffer
-            let mut keep = true;
-            match value.ping_time.elapsed() { //Check Elapsed time on the ping of each client.
-                Ok(elapsed) => {
-                    if elapsed.as_secs() > 10 { //If over 10 secs since last PONG send a PING.
-                        response_bytes.extend_from_slice(&[80,73,78,71]);
-                        response_bytes.extend_from_slice(&value.callsign);
-                        socket.send_to(&response_bytes,value.socket_addr).expect("Error sending PING");
-                        keep = true;
-                    }
-                    if elapsed.as_secs() > 60 { //If over 60 sec since last PONG, DISC the cleint and remove it from the hashmap.
-                        let mut response_bytes = Vec::<u8>::new();
-                        response_bytes.extend_from_slice(&[68,73,83,67]);
-                        response_bytes.extend_from_slice(&value.callsign);
-                        socket.send_to(&response_bytes,value.socket_addr).expect("Error sending DISC");
-                        keep = false;
-                    }
-                }    
-                Err(error) => { //Catch any errors. No idea what error could happen but we need to take care of it.
-                    // an error occurred!
-                    println!("Error: {:?}", error); 
+            match request.url() {
+                "/" => {
+                request.respond(Response::from_file(fs::File::open("Client.html").expect("Error reading Client.html!")));
                 }
+                "/status" => {
+                request.respond(Response::from_string(serde_json::to_string(&Client_list).expect("ERROR")));
+                }
+                _ => {}
             }
-            keep //Return if we keep it or not.
-        });
+            match rx.try_recv(){
+            Ok(clients) => {
+                Client_list = clients;
+               
+            }
+            _ => {}        
+        }
+            //let response = Response::from_string("hello world");
+            //request.respond(response);
+        }
+        
     }
+
+    //handle.join().expect("ERROR JOINING UDP THREAD!");
+
 }
 
 
-fn handle_packet(socket: &UdpSocket,addr:SocketAddr,buf: &[u8;54],clients: &mut HashMap::<SocketAddr,Client> ) {
+
+fn udp_thread(message_out: mpsc::Sender<Vec::<ClientInfo>>) {
+    
+        let socket = UdpSocket::bind("0.0.0.0:17000").expect("couldn't bind to address"); //Get a UDP socket setup
+        let mut buf = [0; 54]; //Buffer for holding packets. M17 IP packets are never longer then 54 bytes.
+        let mut clients =  HashMap::<SocketAddr,Client>::new(); //Hashmap to hold Client Socket relations. 
+        let mut response_bytes = Vec::<u8>::new(); //Holds the response to be sent.
+
+        loop {
+            let (_number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data"); //Recv bytes from the socket.
+            //If the message is to long for the buffer the extra bytes are dropped.
+                                            
+            handle_packet(&socket,src_addr,&buf,&mut clients); //Call out packet handler. 
+            //Borrow out socket,buffer and client hashmap. Just give it the src_addr.
+
+            clients.retain(|_key, value| { //Used for removing clients that timed out.
+                response_bytes.clear(); //Clear the response buffer
+                let mut keep = true;
+                match value.ping_time.elapsed() { //Check Elapsed time on the ping of each client.
+                    Ok(elapsed) => {
+                        if elapsed.as_secs() > 10 { //If over 10 secs since last PONG send a PING.
+                            response_bytes.extend_from_slice(&[80,73,78,71]);
+                            response_bytes.extend_from_slice(&value.callsign);
+                            socket.send_to(&response_bytes,value.socket_addr).expect("Error sending PING");
+                            value.ping();
+                            keep = true;
+                        }
+                        if elapsed.as_secs() > 60 { //If over 60 sec since last PONG, DISC the cleint and remove it from the hashmap.
+                            let mut response_bytes = Vec::<u8>::new();
+                            response_bytes.extend_from_slice(&[68,73,83,67]);
+                            response_bytes.extend_from_slice(&value.callsign);
+                            socket.send_to(&response_bytes,value.socket_addr).expect("Error sending DISC");
+                            keep = false;
+                        }
+                    }    
+                    Err(error) => { //Catch any errors. No idea what error could happen but we need to take care of it.
+                        // an error occurred!
+                        println!("Error: {:?}", error); 
+                    }
+                }
+                keep //Return if we keep it or not.
+            });
+            
+            for (_key, value) in &clients { //Loop over all the clients.
+                let mut clients_info = Vec::<ClientInfo>::new();
+                clients_info.push(ClientInfo{callsign: value.callsign,module: value.module,talking: value.talking});
+                message_out.send(clients_info).expect("Error syncing client info.");
+            }
+        
+        }
+}
+
+
+fn handle_packet(socket: &UdpSocket,addr:SocketAddr,buf: &[u8;54],clients: &mut HashMap::<SocketAddr,Client>) {
     let mut response_bytes = Vec::<u8>::new(); //Buffer for responses.
     match buf {
         [67,79,78,78, ..] => { //Handle CONN packets
@@ -89,6 +156,7 @@ fn handle_packet(socket: &UdpSocket,addr:SocketAddr,buf: &[u8;54],clients: &mut 
             socket.send_to(&response_bytes,addr).expect("Error sending PING"); //Send Ping
         },
         [80,79,78,71, ..] => { //Handle PONG packets
+            //println!("PONG!");
             match clients.get_mut(&addr){ //Get client from the hashmap
                 Some(client) => { //Client existed
                     if client.pingged { //Did we actually ping it or is it blindly sending pongs?
@@ -123,21 +191,7 @@ fn handle_packet(socket: &UdpSocket,addr:SocketAddr,buf: &[u8;54],clients: &mut 
                 }
             }
         },
-        [73,78,70,79, ..] => { //Handle INFO packets - NON STANDARD - Used for the dashboard to conmunicate to the reflector.
-            //println!("INFO");
-            response_bytes.extend([73,78,70,79]); //Add INFO to the response
-            response_bytes.push(clients.len() as u8); //Get number of clients and add to buffer 
-            for (_key, value) in &*clients { //Loop over all the clients.
-                response_bytes.extend_from_slice(&value.callsign); //Send the 6 bytes for the callsign
-                response_bytes.push(value.module); //Send the single byte module
-                if value.talking{
-                    response_bytes.push(1);
-                }else{
-                    response_bytes.push(0);                
-                }
-            }
-            socket.send_to(&response_bytes,addr).expect("Error sending INFO");//Send the packet
-        },
+        
         _ => {println!("WERID UNKNOWN PACKET!");}, //Anything else
     }
 }
